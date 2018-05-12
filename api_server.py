0ddf6cd75da0556e36be5cf956afd644db42b63c
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-05-12 18:43:59 -0400

    functionality to list users from the db

--- api_server.py
+++ api_server.py
@@ -25,7 +25,17 @@ class Task(db.Model):
 @app.route('/api/v1/user', methods=['GET'])
 def get_all_users():
 
-	return jsonify({'users': ''})
+    users = User.query.all()
+    output = []
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
+
+    return jsonify({'users': output})
 
 @app.route('/api/v1/user/<public_id>', methods=['GET'])
 def get_one_user(public_id):
